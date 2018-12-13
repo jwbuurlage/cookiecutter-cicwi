666b13795ad336225c3c5bc1df7531e17e227c31
Allard Hendriksen <allard.hendriksen@cwi.nl>
2018-12-13 16:18:54 +0100

    Add example of docstring with parameters

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -10,3 +10,21 @@ def hello_world():
 
     """
     print("Hello world")
+
+
+def documentation_example(a, b):
+    """This sentence briefly describes the function.
+
+    For more information on docstrings, see:
+
+        https://stackoverflow.com/a/24385103
+
+    This function returns a tuple containing the input parameters.
+
+    :param a: this is a first parameter (int)
+    :param b: this is the second parameter (string)
+    :returns: a tuple
+    :rtype: (int, string)
+
+    """
+    return (a,b)
